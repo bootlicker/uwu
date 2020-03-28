de7a39d0ca302d0d7fda1a91b2925f3cd9359d92
Blair Vidakovich <vidak@riseup.net>
2020-03-29 06:05:35 +0800

    extending and improving the ELIZA code i ganked off the net

--- uwu-eliza.lisp
+++ uwu-eliza.lisp
@@ -38,7 +38,29 @@
         (first alist)
       (lookup key (rest alist)))))
 
-(defparameter *viewpoint* '((I you) (you I) (me you) (am are) (was were) (my your)))
+(defparameter *viewpoint* '(
+			    (I you)
+			    (you I)
+
+			    (me you)
+			    (you me)
+			    
+			    (am are)
+			    (are am)
+			    
+			    (was were)
+			    (were was)
+			    
+			    (my your)
+			    (your my)
+			    
+			    (myself yourself)
+			    (yourself myself)
+			    
+			    (I'm you're)
+			    (you're I'm)
+
+			    ))
 
 (defun swap (value)
   (let* ((a (lookup value *viewpoint*)))
@@ -53,18 +75,47 @@
         (cons (first list) (subs (rest list)))))))
 
 (defparameter *rules*
-  '(((* x hello * y) (hello. how can I help ?))
-    ((* x i want * y) (what would it mean if you got y ?) (why do you want y ?))
-    ((* x i wish * y) (why would it be better if y ?))
-    ((* x i hate * y) (what makes you hate y ?))
+  '(((* x hello * y)
+     (hewwo ^w^  how awe you ?)
+     (gweetings ^w^)
+     (sawutations UwU)
+     (howdy~~ >w<)
+     (wewwcome!!! XD)
+     (hi ya~~)
+     (good day, hooman!)
+     (^.^ what's up ?)
+     (hey~~ o.o)
+     (awwo!! wass happenying ?))
+    
+    ((* x i want * y)
+     (what it mean if u pwocuwed y ?)
+     (OwO why do u wequiwe y ??))
+    
+    ((* x i wish * y)
+     (why would it be better if y ?))
+    
+    ((* x i hate * y)
+     (what makes you hate y ?))
+    
     ((* x if * y)
      (do you really think it is likely that y)
      (what do you think about y))
-    ((* x no * y) (why not?))
-    ((* x i was * y) (why do you say x you were y ?))
-    ((* x i feel * y) (do you often feel y ?))
-    ((* x i felt * y) (what other feelings do you have?))
-    ((* x) (you say x ?) (tell me more.))))
+    
+    ((* x no * y)
+     (why not?))
+    
+    ((* x i was * y)
+     (why do you say x you were y ?))
+    
+    ((* x i feel * y)
+     (do you often feel y ?))
+    
+    ((* x i felt * y)
+     (what other feelings do you have?))
+    
+    ((* x)
+     (you say x ?)
+     (tell me more.))))
 
 (defun random-elt (list)
   (nth (random (length list)) list))
