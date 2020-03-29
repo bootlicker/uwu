9dadb075ca4b4ef1bc9ac0d83b63c1a450c6a150
Blair Vidakovich <vidak@riseup.net>
2020-03-29 15:25:06 +0800

    more of the ELIZA chatbot dictionary

--- uwu-eliza.lisp
+++ uwu-eliza.lisp
@@ -75,17 +75,7 @@
         (cons (first list) (subs (rest list)))))))
 
 (defparameter *rules*
-  '(((* x hello * y)
-     (hewwo ^w^  how awe you ?)
-     (gweetings ^w^)
-     (sawutations UwU)
-     (howdy~~ >w<)
-     (wewwcome!!! XD)
-     (hi ya~~)
-     (good day, hooman!)
-     (^.^ what's up ?)
-     (hey~~ o.o)
-     (awwo!! wass happenying ?))
+  '(
 
 ;;; can you
     
@@ -112,6 +102,12 @@
 
 ;;; you're
 
+    ((* x you are * y)
+     (what makes you think i am y)
+     (does it make u happy to believe i am y)
+     (perhaps you would like to be y)
+     (do you sometimes desiwwe to be))
+
 ;;; i don't
 
     ((* x i don't * y)
@@ -130,8 +126,8 @@
      (does feewing y bother u ? owo'' ??)
      (wat r u aware of wen u feel y ??))
 
-;;; i felt
-
+    ;;; i felt
+    
     ((* x i felt * y)
      (wat othew feewings do you have?)
      (ooo... OWO~~! teww me mowe!!))
@@ -154,34 +150,40 @@
     ;;; are you
 
     ((* x are you * y)
-     ())
-    
-
+     (are you interested in whether or not i am y ?)
+     (would you prefer if i were not y ?)
+     (perhaps you dream that i am y ?))
     
     ;;; i can't
 
     ((* x i cant * y)
-     ())
-
-
+     (how do you know you cant y ?)
+     (have you tried?)
+     (perhaps you can now y ))
     
     ;;; i am
 
     ((* x i am * y)
-     ())
-
+     (did you wanna talk to me because you are y ?)
+     (how long have you been y ?)
+     (do you wweckon its normal to be y ?)
+     (do you enjoy being y ?))
     
     ;;; i'm
 
     ((* x im * y)
-     ())
-
-    
+     (did you wanna talk to me because you are y ?)
+     (how long have you been y ?)
+     (do you wweckon its normal to be y ?)
+     (do you enjoy being y ?))
+        
     ;;; you
 
     ((* x you * y)
-     ())
-
+     (OwO oh yeah??)
+     (oh, i y)
+     (are you wweawwy talking about me ? >w<)
+     (awwe we discussing u or me? XD))
     
     ;;; i want
 
@@ -195,88 +197,100 @@
     
     ;;; what
 
-    (()
-     ())
-
-
+    ((* x what * y)
+     (y do u awwsk ??)
+     (does that interest you ?)
+     (what answer would please you the most ?)
+     (what do you think ?)
+     (is this on your mind often ?)
+     (what do you really want to know ?)
+     (have you asked anyone else about this ?)
+     (have you asked this before ?)
+     (what else comes to mind when you ask that ?)
+     (are you asking me ? ^_^))
     
     ;;; how
 
-    (()
+    ((* x how * y)
      ())
 
     
     ;;; who
 
-    (()
+    ((* x who * y)
      ())
 
     
     ;;; where
 
-    (()
+    ((* x where * y)
      ())
 
     
     ;;; when
 
-    (()
+    ((* x when * y)
      ())
 
     
     ;;; why
 
 
-    (()
+    ((* x why * y)
      ())
 
-    
-    ;;; name
-
-    (()
-     ())
-
-
-    
-    ;;; names
-
-    (()
-     ())
-
-    
     ;;; cause
 
-    (()
-     ())
+    ((* x cause * y)
+     (is that the real reason ?)
+     (do any other reasons come to mind ?)
+     (does that reason explain anything else ?)
+     (what other reasons might there be ?))
 
     
     ;;; because
 
-    (()
-     ())
-
+    ((* x because * y)
+     (is that the real reason ?)
+     (do any other reasons come to mind ?)
+     (does that reason explain anything else ?)
+     (what other reasons might there be ?))
     
     ;;; sorry
 
-    (()
-     ())
+    ((* x sorry * y)
+     (dont apologise, iss kay-o ^w^)
+     (apologies are not necessary !!)
+     (you say sowwee... what makes you feel the need to apologise ?))
 
     
     ;;; dream
 
-    (()
-     ())
-
-    
+    ((* x dream * y)
+     (what does that dream suggest to you ?)
+     (do you often dream ?)
+     (what appears in your dreams ?)
+     (are you made upset by your dwweams? ?))
+   
     ;;; dreams
 
-    (()
+    ((* x dreams * y)
      ())
 
     
     ;;; hello
 
-
+    ((* x hello * y)
+     (hewwo ^w^  how awwe you ?)
+     (gweetings ^w^)
+     (sawutations UwU)
+     (howdy~~ >w<)
+     (wewwcome!!! XD)
+     (hi ya~~)
+     (good day, hooman!)
+     (^.^ what's up ?)
+     (hey~~ o.o)
+     (awwo!! wass happenying ?))
     
     ;;; hi
 
@@ -284,9 +298,11 @@
     
     ;;; maybe
 
-    (()
-     ())
-
+    ((* x maybe * y)
+     (you dont seem quite certain)
+     (why are you uncertain ?)
+     (you are not sure)
+     (dont you know ???))
     
     ;;; no
 
@@ -299,34 +315,45 @@
     
     ;;; your
 
-    (()
-     ())
+    ((* x your * y)
+     (are you concerned about my y ?)
+     (what about your own y ?))
 
     
     ;;; always
     
-    (()
-     ())
+    ((* x always * y)
+     (can you think of a specific example ?)
+     (when ?)
+     (what are you thinking of ?)
+     (really ?? always ??))
 
     
     ;;; think
 
-    (()
-     ())
+    ((* x think * y)
+     (do you really think so ?)
+     (but you are not sure you y ?)
+     (do you doubt you y ?))
 
     
     ;;; alike
 
-    (()
-     ())
+    ((* x alike * y)
+     (in what way ?)
+     (what resemblance do you see ?)
+     (what other connections do you see ?)
+     (how ?))
 
     
     ;;; yes
     
-    (()
-     ())
-    
-
+    ((* x yes * y)
+     (you seem positive ^.^)
+     (are you sure ?)
+     (i see !!)
+     (i understand))
+ 
     
     ((* x i wish * y)
      (why would it be better if y ?))
@@ -341,10 +368,6 @@
     
     ((* x i was * y)
      (why do you say x you were y ?))
-    
-
-    
-
     
     ((* x)
      (you say x ?)
